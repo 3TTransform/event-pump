e7408ea654b6e8611729a7540a0703a5454ed49c
Paul Jones <pauljones13@users.noreply.github.com>
2023-03-16 11:41:18 +0000

    Update test descriptions

--- src/tests/template.test.ts
+++ src/tests/template.test.ts
@@ -3,18 +3,18 @@ import test from "ava";
 import { replaceValues } from "../template";
 
 
-test("replaceValues", async (t) => {
+test("🍏 replaceValues", async (t) => {
   let result = replaceValues( {cakeType: 1,cakeExists: false},
     "cakeType: {{#removeLastChar}}{{cakeType}}, {{cakeExists}}, {{filling}} {{/removeLastChar}}");
   t.assert(result, "cakeType: 1, false");
 });
 
-test("removeLastChar exists", async (t) => {
+test("🍏 removeLastChar exists", async (t) => {
   const Handlebars = require("handlebars");
   t.truthy(Handlebars.helpers.removeLastChar);
 });
 
-test("commadelimlist exists", async (t) => {
+test("🍏 commadelimlist exists", async (t) => {
   const Handlebars = require("handlebars");
   t.truthy(Handlebars.helpers.commadelimlist);
 });
