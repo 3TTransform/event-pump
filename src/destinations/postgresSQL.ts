13a12eb41898a240792b4499acb46c3baaab9b4a
Andy O'Toole <andrewotoole79@gmail.com>
2023-05-16 13:50:01 +0100

    added function to execute a query

--- src/destinations/postgresSQL.ts
+++ src/destinations/postgresSQL.ts
@@ -1,5 +1,5 @@
-import { Pool, Client } from 'pg';
-require('dotenv').config();
+import { Pool, Client } from "pg";
+require("dotenv").config();
 
 // Create a new PostgreSQL client
 const client = new Client({
@@ -7,25 +7,44 @@ const client = new Client({
   password: process.env.PG_PASS,
   host: process.env.PG_HOST,
   database: process.env.PG_DB,
-  port: process.env.PG_PORT // Default PostgreSQL port
+  port: process.env.PG_PORT, // Default PostgreSQL port
 });
 
 // Connect to the PostgreSQL server
 client.connect((err) => {
   if (err) {
-    console.error('Error connecting to PostgreSQL:', err);
+    console.error("Error connecting to PostgreSQL:", err);
     return;
   }
-  console.log('Connected to PostgreSQL!');
+  console.log("Connected to PostgreSQL!");
 
   // Execute queries or perform operations on the database here
 
   // Disconnect from the PostgreSQL server
   client.end((endErr) => {
     if (endErr) {
-      console.error('Error disconnecting from PostgreSQL:', endErr);
+      console.error("Error disconnecting from PostgreSQL:", endErr);
       return;
     }
-    console.log('Disconnected from PostgreSQL.');
+    console.log("Disconnected from PostgreSQL.");
   });
+
+  // Function to execute the PostgreSQL query
+  async function executeQuery(query: string, params?: any[]): Promise<any> {
+    const connection = await client.connect();
+
+    try {
+      const result = await connection.query(query, params);
+      return result.rows;
+    } finally {
+      connection.release();
+    }
+  }
+
+  // Example usage
+  async function getUsers(): Promise<any[]> {
+    const query = "SELECT * FROM users";
+    const users = await executeQuery(query);
+    return users;
+  }
 });
