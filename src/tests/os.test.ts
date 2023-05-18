93816d095f64e45f38cdadb47adca11ff7f1d824
Andy O'Toole <andrewotoole79@gmail.com>
2023-05-18 15:30:26 +0100

    turned off dyndb and os tests

--- src/tests/os.test.ts
+++ src/tests/os.test.ts
@@ -2,7 +2,7 @@ import test from 'ava';
 
 import { handler } from '../destinations/os';
 
-test('🍏 handler', async (t) => {
-    const result = await handler('search', 'event');
-    t.assert(result.body.hits.hits[0]._source.name, 'Not Harry Potter');
-});
+// test('🍏 handler', async (t) => {
+//     const result = await handler('search', 'event');
+//     t.assert(result.body.hits.hits[0]._source.name, 'Not Harry Potter');
+// });
