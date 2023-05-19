c9a2a6acec8ca33a519eaffad7266e929dc87a0d
IstvanFranko <125575995+IstvanFranko@users.noreply.github.com>
2023-05-19 12:37:18 +0100

    Updated OpenSearch

--- src/destinations/os.ts
+++ src/destinations/os.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 import { Client } from '@opensearch-project/opensearch';
+import { populateEventData } from '../utils';
 
 const { 
     OS_URL: osURL, 
@@ -9,112 +10,160 @@ const {
 } = process.env;
 const protocol = 'https';
 
-const handler = async (pattern: unknown, event: unknown) => {
-    const client = new Client({
-        node: protocol + '://' + osUser + ':' + osPassword + '@' + osURL,
-        // use_ssl: true,
-    });
+const client = new Client({
+    node: protocol + '://' + osUser + ':' + osPassword + '@' + osURL,
+    // use_ssl: true,
+});
 
-    console.log(pattern);
-    console.log(event);
+const openSearchHydrateOne = async (pattern: any, event: any, isFirstEvent: boolean) => {
 
-    const index_name = 'not_books';
-    const id = '2';    
+    const singleItem = populateEventData(event, pattern.action.params.Item);
+    const index_name = pattern.action.params.TableName?.toLowerCase();
 
-    // Create an index with non-default settings
-    if (pattern == 'create_index') {
-        const settings = {
-            settings: {
-                index: {
-                    number_of_shards: 4,
-                    number_of_replicas: 3,
-                },
-            },
-        };
-          
-        const response = await client.indices.create({
-            index: index_name,
-            body: settings,
-        });
-
-        console.log('Creating index:');
-        console.log(response.body);
-
-        return response;
+    if (!index_name) {
+        console.log('OS: table name missing');
+        return;
     }
-
-    // Add a document to the index.
-    if (pattern == 'create_item') {
-        const document = {
-            name: 'The Outsider',
-        };
-
-        const response = await client.index({
-            id: id,
-            index: index_name,
-            body: document,
-            refresh: true,
-        });
-
-        console.log('Creating item:');
-        console.log(response.body);
-
-        return response;
-    }
-
-    // Search for the document.
-    if (pattern == 'search') {
-        const query = {
-            query: {
-                match: {
-                    name: {
-                        query: 'Not Harry Potter',
-                    },
-                },
-            },
-        };
     
-        const response = await client.search({
-            index: index_name,
-            body: query,
-        });
+    if (!(await client.indices.exists({index: index_name })).body)
+    {
+        // create index
+        await osCreateIndex(index_name);
+    }
     
-        console.log('Search results:');
-        console.log(response.body.hits.hits[0]._source);
-
-        return response;
+    switch (pattern.rule.verb) {
+    case 'create':
+        await osCreate(singleItem, index_name);
+        break;
+    case 'delete':
+        await osDelete(singleItem, index_name);
+        break;
+    case 'update':
+        await osUpdate(singleItem, index_name);
+        break;
+    case 'search':
+        await osSearch(index_name);
+        break;
+    default:
+        throw new Error(
+            `Action target ${pattern.action.target} is not supported`
+        );
     }
+       
 
-    // Delete the document.
-    if (pattern == 'delete_item') {
-
-        const id = '2';
-
-        const response = await client.delete({
-            index: index_name,
-            id: id,
-        });
-        
-        console.log('Deleting item:');
-        console.log(response.body);
-
-        return response;
-    }
-
-    // Delete the index.
-    if (pattern == 'delete_index') {
-
-        const response = await client.indices.delete({
-            index: index_name,
-        });
-        
-        console.log('Deleting index:');
-        console.log(response.body);
-
-        return response;
-    }
 
     return null;    
 };
 
-export { handler };
+async function osCreate(singleItem: any, index_name: string) {    
+
+    const document = {};
+
+    for (const [key, value] of Object.entries(singleItem)) {
+        if (key != 'id') {
+            document[key] = value;
+        }
+    }
+
+    return await client.index({
+        id: singleItem.id,
+        index: index_name,
+        body: document,
+        refresh: true,
+    });
+}
+
+async function osCreateIndex(index_name: string) {
+    // Create an index with non-default settings
+    const settings = {
+        settings: {
+            index: {
+                number_of_shards: 1,  //4
+                number_of_replicas: 0,  //3
+            },
+        },
+    };
+      
+    return await client.indices.create({
+        index: index_name,
+        body: settings,
+    });
+}
+
+async function osDelete(singleItem: any, index_name: string) {
+    // Delete the document.
+    const response = await client.delete({
+        index: index_name,
+        id: singleItem.id,
+    });
+
+    return response;
+}
+
+async function osDeleteIndex(singleItem: any, index_name: string) {
+    // Delete the index.
+    const response = await client.indices.delete({
+        index: index_name,
+    });
+    
+    console.log('Deleting index:');
+    console.log(response.body);
+
+    return response;
+}
+
+async function osUpdate(singleItem: any, index_name: string) {
+
+    const document = {};
+
+    for (const [key, value] of Object.entries(singleItem)) {
+        if (key != 'id') {
+            document[key] = value;
+        }
+    }
+    
+    return await client.update({
+        id: singleItem.id,
+        index: index_name,
+        body: {
+            doc: document
+        }
+    });
+}
+
+async function osSearch(index_name: string) {
+    // Search for the document.
+    // const query = {
+    //     query: {
+    //         match: {
+    //             name: {
+    //                 query: 'The Outsider',
+    //             },
+    //         },
+    //     },
+    // };
+
+    const query = {
+        size: 100, //default 10
+        query: {
+            match_all: {}
+        },
+        // sort: [
+        //     {
+        //         organisationName: {
+        //             order: 'desc'
+        //         }
+        //     }
+        // ]
+    };    
+
+    const response = await client.search({
+        index: index_name,
+        body: query,
+    });
+    //console.log(response.body.hits.hits[0]._source);
+    console.log(response.body.hits.hits);
+    return response;
+}
+
+export { openSearchHydrateOne, osSearch };
