22f4f8432bcb56bfae98b476fc66715a0de5a47c
Paul Jones <pauljones13@users.noreply.github.com>
2023-05-23 17:23:37 +0100

    Reduce duplication

--- src/tests/utils.fs.test.ts
+++ src/tests/utils.fs.test.ts
@@ -3,147 +3,93 @@ import fs from 'fs';
 import sinon from 'sinon';
 import { createFolderFromPath, blankFileIfExists } from '../utils';
 
+let existsSyncStub;
+let mkdirSyncStub;
+let writeFileSyncStub;
+
+test.beforeEach(()=>{
+  existsSyncStub = sinon.stub(fs, 'existsSync');
+  mkdirSyncStub = sinon.stub(fs, 'mkdirSync');
+  writeFileSyncStub = sinon.stub(fs, 'writeFileSync');
+});
+test.afterEach(()=>{
+  existsSyncStub.restore();
+  mkdirSyncStub.restore();
+  writeFileSyncStub.restore();
+});
+
 test.serial('🍏 createFolderFromPath should create the folder if it does not exist', (t) => {
-  const existsSyncStub = sinon.stub(fs, 'existsSync').returns(false);
-  const mkdirSyncStub = sinon.stub(fs, 'mkdirSync');
-
+  existsSyncStub.returns(false);
   createFolderFromPath('path/to/file.txt');
-
   t.true(existsSyncStub.calledOnceWithExactly('path/to'));
   t.true(mkdirSyncStub.calledOnceWithExactly('path/to', { recursive: true }));
-
-  existsSyncStub.restore();
-  mkdirSyncStub.restore();
 });
 test.serial('🍎 createFolderFromPath should not create the folder if it already exists', (t) => {
-  const existsSyncStub = sinon.stub(fs, 'existsSync').returns(true);
-  const mkdirSyncStub = sinon.stub(fs, 'mkdirSync');
-
+  existsSyncStub.returns(true);
   createFolderFromPath('existing/folder/file.txt');
-
   t.true(existsSyncStub.calledOnceWithExactly('existing/folder'));
   t.true(mkdirSyncStub.notCalled);
-
-  existsSyncStub.restore();
-  mkdirSyncStub.restore();
 });
 test.serial('🍎 createFolderFromPath should not create the folder if the filename does not contain any slashes', (t) => {
-  const existsSyncStub = sinon.stub(fs, 'existsSync');
-  const mkdirSyncStub = sinon.stub(fs, 'mkdirSync');
-
   createFolderFromPath('filename.txt');
-
   t.true(existsSyncStub.notCalled);
   t.true(mkdirSyncStub.notCalled);
-
-  existsSyncStub.restore();
-  mkdirSyncStub.restore();
 });
 test.serial('🍎 createFolderFromPath should not create the folder if the filename is null', (t) => {
-  const existsSyncStub = sinon.stub(fs, 'existsSync');
-  const mkdirSyncStub = sinon.stub(fs, 'mkdirSync');
-
   createFolderFromPath(null);
-
   t.true(existsSyncStub.notCalled);
   t.true(mkdirSyncStub.notCalled);
-
-  existsSyncStub.restore();
-  mkdirSyncStub.restore();
 });
 test.serial('🍎 createFolderFromPath should not create the folder if the filename is undefined', (t) => {
-  const existsSyncStub = sinon.stub(fs, 'existsSync');
-  const mkdirSyncStub = sinon.stub(fs, 'mkdirSync');
-
   createFolderFromPath(undefined);
-
   t.true(existsSyncStub.notCalled);
   t.true(mkdirSyncStub.notCalled);
-
-  existsSyncStub.restore();
-  mkdirSyncStub.restore();
 });
 test.serial('🍎 createFolderFromPath should not create the folder if the filename is empty', (t) => {
-  const existsSyncStub = sinon.stub(fs, 'existsSync');
-  const mkdirSyncStub = sinon.stub(fs, 'mkdirSync');
-
   createFolderFromPath('');
-
   t.true(existsSyncStub.notCalled);
   t.true(mkdirSyncStub.notCalled);
-
-  existsSyncStub.restore();
-  mkdirSyncStub.restore();
 });
 
 test.serial('🍏 blankFileIfExists should blank the file and return true if it exists', (t) => {
   const filename = 'path/to/file.txt';
-  const existsSyncStub = sinon.stub(fs, 'existsSync').returns(true);
-  const writeFileSyncStub = sinon.stub(fs, 'writeFileSync');
-
+  existsSyncStub.returns(true);
   const result = blankFileIfExists(filename);
-
   t.true(existsSyncStub.calledOnceWithExactly(filename));
   t.true(writeFileSyncStub.calledOnceWithExactly(filename, ''));
   t.true(result);
-
-  existsSyncStub.restore();
-  writeFileSyncStub.restore();
 });
+
+
 test.serial('🍎 blankFileIfExists should return false if the file does not exist', (t) => {
   const filename = 'nonexistent/file.txt';
-  const existsSyncStub = sinon.stub(fs, 'existsSync').returns(false);
-  const writeFileSyncStub = sinon.stub(fs, 'writeFileSync');
-
+  existsSyncStub.returns(false);
   const result = blankFileIfExists(filename);
-
   t.true(existsSyncStub.calledOnceWithExactly(filename));
   t.true(writeFileSyncStub.notCalled);
   t.false(result);
-
-  existsSyncStub.restore();
-  writeFileSyncStub.restore();
 });
-
 test.serial('🍎 blankFileIfExists should return false if the filename is null', (t) => {
   const filename = null;
-  const existsSyncStub = sinon.stub(fs, 'existsSync').returns(false);
-  const writeFileSyncStub = sinon.stub(fs, 'writeFileSync');
-
+  existsSyncStub.returns(false);
   const result = blankFileIfExists(filename);
-
   t.true(existsSyncStub.calledOnceWithExactly(filename));
   t.true(writeFileSyncStub.notCalled);
   t.false(result);
-
-  existsSyncStub.restore();
-  writeFileSyncStub.restore();
 });
 test.serial('🍎 blankFileIfExists should return false if the filename is undefined', (t) => {
   const filename = undefined;
-  const existsSyncStub = sinon.stub(fs, 'existsSync').returns(false);
-  const writeFileSyncStub = sinon.stub(fs, 'writeFileSync');
-
+  existsSyncStub.returns(false);
   const result = blankFileIfExists(filename);
-
   t.true(existsSyncStub.calledOnceWithExactly(filename));
   t.true(writeFileSyncStub.notCalled);
   t.false(result);
-
-  existsSyncStub.restore();
-  writeFileSyncStub.restore();
 });
 test.serial('🍎 blankFileIfExists should return false if the filename is empty', (t) => {
   const filename = '';
-  const existsSyncStub = sinon.stub(fs, 'existsSync').returns(false);
-  const writeFileSyncStub = sinon.stub(fs, 'writeFileSync');
-
+  existsSyncStub.returns(false);
   const result = blankFileIfExists(filename);
-
   t.true(existsSyncStub.calledOnceWithExactly(filename));
   t.true(writeFileSyncStub.notCalled);
   t.false(result);
-
-  existsSyncStub.restore();
-  writeFileSyncStub.restore();
 });
