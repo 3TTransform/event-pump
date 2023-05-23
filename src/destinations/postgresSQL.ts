defca9cc5bfbc1f39efff9357cd2809791b22251
rajeshlakkaraju <125990088+rajeshlakkaraju@users.noreply.github.com>
2023-05-23 11:20:13 +0100

    mob 23rd May
    update to the postgress connections

--- src/destinations/postgresSQL.ts
+++ src/destinations/postgresSQL.ts
@@ -1,52 +1,37 @@
-import { Client } from "pg";
+import { Client, Pool } from "pg";
 import { populateEventData } from "../utils";
 import { replaceValues } from "../template";
 import { error } from "console";
 require("dotenv").config();
 
 // Create a new PostgreSQL client
-const client = new Client({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASS,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DB,
-  port: process.env.PG_PORT, // Default PostgreSQL port
-});
+// const client = new Client({
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASS,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DB,
+//   port: process.env.PG_PORT, // Default PostgreSQL port
+// });
+
+let connection;
 
 const executeQuery = async (query: string, params?: any[]): Promise<any> => {
-  console.log("😍");
-  const connection = await client.connect();
-  console.log("😄");
-  console.log();
-  try {
-    const result = await connection.query(query, params);
-    return result.rows;
-  } catch (error) {
-    console.log("😡");
-    console.log(error);
-  } finally {
-    connection.release();
-  }
+  const pool = new Pool({
+    user: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    port: process.env.PG_PORT
+  });
+
+  console.log("🙏");
+  pool.query(query, [], (error2, data) => {
+    console.log("😄");
+    console.log(error2, data);
+    pool.end();
+  });
+ 
 };
-
-  // Connect to the PostgreSQL server
-  // client.connect((err) => {
-  //   if (err) {
-  //     console.error("Error connecting to PostgreSQL:", err);
-  //     return;
-  //   }
-  //   console.log("Connected to PostgreSQL!");
-
-  // // Disconnect from the PostgreSQL server
-  // client.end((endErr) => {
-  //   if (endErr) {
-  //     console.error("Error disconnecting from PostgreSQL:", endErr);
-  //     return;
-  //   }
-  //   console.log("Disconnected from PostgreSQL.");
-  // });
-  // Execute queries or perform operations on the database here
-// });
 
 export const postgresSqlHydrateOne = async (
   pattern: any,
@@ -57,11 +42,15 @@ export const postgresSqlHydrateOne = async (
     event,
     pattern.action.params.input
   );
+  console.log(`connection = ${connection}`);
 
   let replacedSQL = replaceValues(event, pattern.action.params.sql);
   replacedSQL = replacedSQL.replace(/,\s*WHERE/g, " WHERE");
-  console.log("🙏");
+  //replacedSQL = "SELECT 5 AS COLUMN_ONE, 'col 2 value' AS COLUMN_TWO";
+  
   try {
+    console.log(`replacedSQL = ${replacedSQL}`);
+    console.log(`populatedParameters = ${populatedParameters}`);
     await executeQuery(replacedSQL, populatedParameters);
   } catch (err) {
     console.log(`${event.id} failed ${err.message}`);
