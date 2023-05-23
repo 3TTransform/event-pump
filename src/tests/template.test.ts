12e35ed5545491434c5408c5d02c4fd824d3db61
Paul Jones <pauljones13@users.noreply.github.com>
2023-05-23 15:40:32 +0100

    Tests for template.ts

--- src/tests/template.test.ts
+++ src/tests/template.test.ts
@@ -3,16 +3,55 @@ import Handlebars from 'handlebars';
 import { replaceValues } from '../template';
 
 
-test('🍏 replaceValues', async (t) => {
-    const result = replaceValues( {cakeType: 1,cakeExists: false},
-        'cakeType: {{#removeLastChar}}{{cakeType}}, {{cakeExists}}, {{filling}} {{/removeLastChar}}');
-    t.assert(result, 'cakeType: 1, false');
+test('🍏 replaceValues should replace values correctly (1)', async (t) => {
+  const result = replaceValues({ cakeType: 1, cakeExists: false },
+    'cakeType: {{#removeLastChar}}{{cakeType}}, {{cakeExists}}, {{filling}} {{/removeLastChar}}');
+  t.assert(result, 'cakeType: 1, false');
 });
 
-test('🍏 removeLastChar exists', async (t) => {
-    t.truthy(Handlebars.helpers.removeLastChar);
+
+test('🍏 replaceValues should replace values correctly (2)', (t) => {
+  const data = {
+    name: 'John Doe',
+    age: 25,
+  };
+  const source = 'My name is {{name}} and I am {{age}} years old.';
+  const expected = 'My name is John Doe and I am 25 years old.';
+
+  const result = replaceValues(data, source);
+
+  t.is(result, expected);
 });
 
-test('🍏 commadelimlist exists', async (t) => {
-    t.truthy(Handlebars.helpers.commadelimlist);
+test('🍎 replaceValues should handle missing values', (t) => {
+  const data = {
+    name: 'John Doe',
+  };
+  const source = 'My name is {{name}} and I am {{age}} years old.';
+  const expected = 'My name is John Doe and I am  years old.';
+
+  const result = replaceValues(data, source);
+
+  t.is(result, expected);
+});
+
+test('🍎 replaceValues should handle multiple occurrences of the same value', (t) => {
+  const data = {
+    name: 'John Doe',
+    age: 25,
+  };
+  const source = '{{name}}, {{name}}, {{name}}';
+  const expected = 'John Doe, John Doe, John Doe';
+
+  const result = replaceValues(data, source);
+
+  t.is(result, expected);
+});
+
+test('🍏 Handlebars Helper: removeLastChar exists', async (t) => {
+  t.truthy(Handlebars.helpers.removeLastChar);
+});
+
+test('🍏 Handlebars Helper: commadelimlist exists', async (t) => {
+  t.truthy(Handlebars.helpers.commadelimlist);
 });
