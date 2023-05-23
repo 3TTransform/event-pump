cb6fd5bc22ec170dbc8584c2ad7a16380923a6b2
Andy O'Toole <andrewotoole79@gmail.com>
2023-05-23 16:01:42 +0100

    got postgres working

--- src/destinations/postgresSQL.ts
+++ src/destinations/postgresSQL.ts
@@ -1,36 +1,26 @@
-import { Client, Pool } from "pg";
+import { Pool, Client } from "pg";
 import { populateEventData } from "../utils";
 import { replaceValues } from "../template";
-import { error } from "console";
 require("dotenv").config();
-
-// Create a new PostgreSQL client
-// const client = new Client({
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASS,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DB,
-//   port: process.env.PG_PORT, // Default PostgreSQL port
-// });
 
 let connection;
 
-const executeQuery = async (query: string, params?: any[]): Promise<any> => {
+const createPool = () => {
   const pool = new Pool({
     user: process.env.PG_USER,
     password: process.env.PG_PASS,
     host: process.env.PG_HOST,
     database: process.env.PG_DB,
-    port: process.env.PG_PORT
+    port: process.env.PG_PORT,
   });
+  return pool;
+};
 
-  console.log("🙏");
-  pool.query(query, [], (error2, data) => {
-    console.log("😄");
-    console.log(error2, data);
-    pool.end();
-  });
- 
+const getPool = () => {
+  if (!connection) {
+    connection = createPool();
+  }
+  return connection;
 };
 
 export const postgresSqlHydrateOne = async (
@@ -38,20 +28,9 @@ export const postgresSqlHydrateOne = async (
   event: any,
   isFirstEvent: boolean
 ) => {
-  const populatedParameters = populateEventData(
-    event,
-    pattern.action.params.input
-  );
-  console.log(`connection = ${connection}`);
-
-  let replacedSQL = replaceValues(event, pattern.action.params.sql);
-  replacedSQL = replacedSQL.replace(/,\s*WHERE/g, " WHERE");
-  //replacedSQL = "SELECT 5 AS COLUMN_ONE, 'col 2 value' AS COLUMN_TWO";
-  
   try {
-    console.log(`replacedSQL = ${replacedSQL}`);
-    console.log(`populatedParameters = ${populatedParameters}`);
-    await executeQuery(replacedSQL, populatedParameters);
+    let replacedSQL = replaceValues(event, pattern.action.params.sql);
+    await getPool().query(replacedSQL);
   } catch (err) {
     console.log(`${event.id} failed ${err.message}`);
   }
