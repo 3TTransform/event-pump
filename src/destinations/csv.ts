c5f7932b20b9a37a1824d6e37e2c0beec76f0328
Paul Jones <pauljones13@users.noreply.github.com>
2023-05-26 13:31:09 +0100

    Add CSV Reader

---
+++ src/destinations/csv.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import readline from 'readline';
+
+class CSV {
+  constructor(private doc: any) { }
+
+  readEvents = async function* () {
+    const readInterface = readline.createInterface({
+      input: fs.createReadStream(this.doc.source.file)
+    });
+    let headers;
+    for await (const line of readInterface) {
+      const row = line.split(',');
+      if (!headers) {
+        headers = row
+        continue;
+      }
+      yield Object.fromEntries(headers
+        .map((header, index) => {
+          if (row[index]) {
+            return [header.trim(), row[index].trim()]
+          }
+        })
+        .filter(x => x));
+    }
+  }
+
+}
+
+export default CSV;
