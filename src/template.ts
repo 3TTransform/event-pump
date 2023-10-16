eb9e5043d7e28798344abba5de2077af87dd3530
IstvanFranko <125575995+IstvanFranko@users.noreply.github.com>
2023-10-16 14:36:58 +0100

    Add sha256 to handlebars.

--- src/template.ts
+++ src/template.ts
@@ -1,4 +1,5 @@
 import Handlebars from 'handlebars';
+import crypto from 'crypto';
 
 Handlebars.registerHelper('removeLastChar', function (options: any) {
     const result = options.fn(this);
@@ -11,6 +12,21 @@ Handlebars.registerHelper('commadelimlist', function (options: any) {
         result = result.replaceAll(/,\s*,/, ',');
     }
     return result.replace(/,\s*$/, ' ').replace(/^\s*,/, ' ');
+});
+
+// Register a custom Handlebars helper for SHA-256 hashing
+Handlebars.registerHelper('sha256', function (value) {
+    // Check if the value is provided
+    if (typeof value === 'string') {
+        // Create a SHA-256 hash
+        const sha256Hash = crypto
+            .createHash('sha256')
+            .update(value)
+            .digest('hex');
+        return sha256Hash;
+    } else {
+        return ''; // Return an empty string if the value is not provided or invalid
+    }
 });
 
 function replaceValues(data: any, source: any) {
